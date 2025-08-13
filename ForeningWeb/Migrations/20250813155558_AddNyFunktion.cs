8a5d7a598e0296bf21e3b33e925153e5ccaa18aa
Somalim-10 <mohmed43001@gmail.com>
2025-08-13 17:56:56 +0200

    scaffold misslykkedes, manuelt immplementeret

---
+++ ForeningWeb/Migrations/20250813155558_AddNyFunktion.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ForeningWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNyFunktion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Donationer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    MobilePayNummer = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Besked = table.Column<string>(type: "TEXT", nullable: true),
+                    QrKodePath = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Donationer", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Donationer");
+        }
+    }
+}
