8bfac04ed2daac62c8d2047aa62ac51042db6cb8
Somalim-10 <mohmed43001@gmail.com>
2025-10-31 23:23:15 +0100

    fixet og tiføjet flere

--- ForeningWeb.Tests/Models/OmTests.cs
+++ ForeningWeb.Tests/Models/OmTests.cs
@@ -1,11 +1,20 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ForeningWeb.Models;
+using System;
 
 namespace ForeningWeb.Tests.Models
 {
     [TestClass]
     public class OmTests
     {
+        private Om _om;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _om = new Om { Id = 1, Indhold = "Noget indhold", BilledePath = "/img/test.png" };
+        }
+
         [TestMethod]
         public void Om_DefaultValues_AreSet()
         {
@@ -13,6 +22,25 @@ namespace ForeningWeb.Tests.Models
             Assert.AreEqual(0, om.Id);
             Assert.AreEqual(string.Empty, om.Indhold);
             Assert.IsNull(om.BilledePath);
+        }
+
+        [TestMethod]
+        public void Indhold_ValidValue_SetsValue()
+        {
+            _om.Indhold = "Nyt indhold";
+            Assert.AreEqual("Nyt indhold", _om.Indhold);
+        }
+
+        [TestMethod]
+        public void Indhold_Empty_ThrowsArgumentException()
+        {
+            Assert.ThrowsException<ArgumentException>(() => _om.Indhold = "");
+        }
+
+        [TestMethod]
+        public void Id_Negative_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _om.Id = -1);
         }
     }
 }
--- ForeningWeb/Models/Donation.cs
+++ ForeningWeb/Models/Donation.cs
@@ -1,19 +1,48 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ForeningWeb.Models
 {
     public class Donation
     {
-        public int Id { get; set; }
+        private int _id;
+        private string _mobilePayNummer = string.Empty;
+        private string? _besked;
+        private string? _qrKodePath;
 
-        [Required, StringLength(50)]
-        public string MobilePayNummer { get; set; } = string.Empty;
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException(nameof(Id), "Id skal være positivt.");
+                _id = value;
+            }
+        }
 
-        // Tekst der vises ved siden af QR-koden
-        public string? Besked { get; set; }
+        public string MobilePayNummer
+        {
+            get => _mobilePayNummer;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("MobilePayNummer må ikke være tom.", nameof(MobilePayNummer));
+                if (value.Length > 50)
+                    throw new ArgumentException("MobilePayNummer må maks være 50 tegn.", nameof(MobilePayNummer));
+                _mobilePayNummer = value;
+            }
+        }
 
-        // Sti til QR-kode billede
-        public string? QrKodePath { get; set; }
-        //11
+        public string? Besked
+        {
+            get => _besked;
+            set => _besked = value;
+        }
+
+        public string? QrKodePath
+        {
+            get => _qrKodePath;
+            set => _qrKodePath = value;
+        }
     }
 }
